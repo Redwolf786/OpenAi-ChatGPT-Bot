f73ba229e0392f1a143087503b42adc3e44c9f5e
Redwolf786 <61825812+Redwolf786@users.noreply.github.com>
2023-11-07 23:59:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
+global.OWNER_NAME = 'Lucario' // Owner Name
 global.OWNER_NUMBER = ['94715166712'] //Owner Number
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
